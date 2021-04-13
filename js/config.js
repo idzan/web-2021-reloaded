eb5e788a5e4c37c93e86cce7748e324118c13146
Marko Idzan <42409619+idzan@users.noreply.github.com>
2021-04-13 15:04:14 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -33,68 +33,25 @@ var klaroConfig = {
 
     cookieExpiresAfterDays: 120,
 
-    // You can change to cookie domain for the consent manager itself.
-    // Use this if you want to get consent once for multiple matching domains.
-    // If undefined, Klaro will use the current domain.
-    //cookieDomain: '.github.com',
-
-    // You can change to cookie path for the consent manager itself.
-    // Use this to restrict the cookie visibility to a specific path.
-    // If undefined, Klaro will use '/' as cookie path.
-    //cookiePath: '/',
-
-    // Defines the default state for services (true=enabled by default).
     default: false,
 
-    // If "mustConsent" is set to true, Klaro will directly display the consent
-    // manager modal and not allow the user to close it before having actively
-    // consented or declines the use of third-party services.
     mustConsent: false,
 
-    // Show "accept all" to accept all services instead of "ok" that only accepts
-    // required and "default: true" services
-    acceptAll: true,
+    acceptAll: false,
 
-    // replace "decline" with cookie manager modal
     hideDeclineAll: false,
-
-    // hide "learnMore" link
     hideLearnMore: false,
-
-    // show cookie notice as modal
     noticeAsModal: false,
 
-    // You can also remove the 'Realized with Klaro!' text in the consent modal.
-    // Please don't do this! We provide Klaro as a free open source tool.
-    // Placing a link to our website helps us spread the word about it,
-    // which ultimately enables us to make Klaro! better for everyone.
-    // So please be fair and keep the link enabled. Thanks :)
     disablePoweredBy: true,
 
-    // you can specify an additional class (or classes) that will be added to the Klaro `div`
-    //additionalClass: 'my-klaro',
-
-    // You can define the UI language directly here. If undefined, Klaro will
-    // use the value given in the global "lang" variable. If that does
-    // not exist, it will use the value given in the "lang" attribute of your
-    // HTML tag. If that also doesn't exist, it will use 'en'.
     lang: 'en',
 
-    // You can overwrite existing translations and add translations for your
-    // service descriptions and purposes. See `src/translations/` for a full
-    // list of translations that can be overwritten:
-    // https://github.com/KIProtect/klaro/tree/master/src/translations
-
-    // Example config that shows how to overwrite translations:
-    // https://github.com/KIProtect/klaro/blob/master/src/configs/i18n.js
     translations: {
-        // translationsed defined under the 'zz' language code act as default
-        // translations.
+
         zz: {
             privacyPolicyUrl: '/privacy',
         },
-        // If you erase the "consentModal" translations, Klaro will use the
-        // bundled translations.
         en: {
             consentModal: {
                 title: 'Do you agree about our cookies and settings?',
