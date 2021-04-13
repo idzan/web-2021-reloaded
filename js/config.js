da7a92025d28ea0d472c1156ea1a8329da7ef48f
Marko Idzan <42409619+idzan@users.noreply.github.com>
2021-04-13 14:14:17 +0200

    basic klaro config with all required as it is cloudflare exclusive dang it

---
+++ js/config.js
@@ -0,0 +1,128 @@
+var klaroConfig = {
+    // With the 0.7.0 release we introduce a 'version' paramter that will make
+    // if easier for us to keep configuration files backwards-compatible in the future.
+    version: 1,
+
+    // You can customize the ID of the DIV element that Klaro will create
+    // when starting up. If undefined, Klaro will use 'klaro'.
+    elementID: 'klaro',
+
+    // You can override CSS style variables here. For IE11, Klaro will
+    // dynamically inject the variables into the CSS. If you still consider
+    // supporting IE9-10 (which you probably shouldn't) you need to use Klaro
+    // with an external stylesheet as the dynamic replacement won't work there.
+    styling: {
+        theme: ['dark', 'bottom', 'short'],
+    },
+
+    noAutoLoad: false,
+
+    // Setting this to true will render the descriptions of the consent
+    // modal and consent notice are HTML. Use with care.
+    htmlTexts: true,
+
+    // Setting 'embedded' to true will render the Klaro modal and notice without
+    // the modal background, allowing you to e.g. embed them into a specific element
+    // of your website, such as your privacy notice.
+    embedded: false,
+    
+    groupByPurpose: true,
+
+    storageMethod: 'localStorage',
+
+    // You can customize the name of the cookie that Klaro uses for storing
+    // user consent decisions. If undefined, Klaro will use 'klaro'.
+    cookieName: 'idzan_consent',
+
+    cookieExpiresAfterDays: 120,
+
+    // You can change to cookie domain for the consent manager itself.
+    // Use this if you want to get consent once for multiple matching domains.
+    // If undefined, Klaro will use the current domain.
+    //cookieDomain: '.github.com',
+
+    // You can change to cookie path for the consent manager itself.
+    // Use this to restrict the cookie visibility to a specific path.
+    // If undefined, Klaro will use '/' as cookie path.
+    //cookiePath: '/',
+
+    // Defines the default state for services (true=enabled by default).
+    default: false,
+
+    // If "mustConsent" is set to true, Klaro will directly display the consent
+    // manager modal and not allow the user to close it before having actively
+    // consented or declines the use of third-party services.
+    mustConsent: false,
+
+    // Show "accept all" to accept all services instead of "ok" that only accepts
+    // required and "default: true" services
+    acceptAll: true,
+
+    // replace "decline" with cookie manager modal
+    hideDeclineAll: true,
+
+    // hide "learnMore" link
+    hideLearnMore: false,
+
+    // show cookie notice as modal
+    noticeAsModal: false,
+
+    // You can also remove the 'Realized with Klaro!' text in the consent modal.
+    // Please don't do this! We provide Klaro as a free open source tool.
+    // Placing a link to our website helps us spread the word about it,
+    // which ultimately enables us to make Klaro! better for everyone.
+    // So please be fair and keep the link enabled. Thanks :)
+    disablePoweredBy: true,
+
+    // you can specify an additional class (or classes) that will be added to the Klaro `div`
+    //additionalClass: 'my-klaro',
+
+    // You can define the UI language directly here. If undefined, Klaro will
+    // use the value given in the global "lang" variable. If that does
+    // not exist, it will use the value given in the "lang" attribute of your
+    // HTML tag. If that also doesn't exist, it will use 'en'.
+    lang: 'en',
+
+    // You can overwrite existing translations and add translations for your
+    // service descriptions and purposes. See `src/translations/` for a full
+    // list of translations that can be overwritten:
+    // https://github.com/KIProtect/klaro/tree/master/src/translations
+
+    // Example config that shows how to overwrite translations:
+    // https://github.com/KIProtect/klaro/blob/master/src/configs/i18n.js
+    translations: {
+        // translationsed defined under the 'zz' language code act as default
+        // translations.
+        zz: {
+            privacyPolicyUrl: '/privacy',
+        },
+        // If you erase the "consentModal" translations, Klaro will use the
+        // bundled translations.
+        en: {
+            consentModal: {
+                title: '<u>test</u>',
+                description:
+                    'Here you can see and customize the information that we collect about you. Entries marked as "Example" are just for demonstration purposes and are not really used on this website.',
+            },
+            cloudflare: {
+                description: 'Protection against DDoS attacks, Pages (hosting) and basic Analytics',
+            },
+            purposes: {
+                analytics: 'Analytics',
+                essentials: 'Essentials',
+                hosting: 'Hosting and Content Delivery Network',
+                security: 'Security',
+            },
+        },
+    },
+
+    apps: [
+        {
+            name: 'cloudflare',
+            title: 'Cloudflare',
+            purposes: ['analytics', 'hosting', 'security'],
+            required: true,
+            cookies: ['__cf_bm', '__cfduid']
+        }
+    ]
+};
